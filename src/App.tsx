ca8ae4b4c0e56ce30e9fe7dfab4901897158fcbb
mxbialostocki <matt.bialostocki@gmail.com>
2020-10-22 08:12:36 +1300

    Hello, World

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        Hello, World
         </p>
         <a
           className="App-link"
